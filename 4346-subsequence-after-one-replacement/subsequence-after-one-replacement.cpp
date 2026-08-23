4595ee4422b15bd38ed47336171423e575de9e06
Priyanka Gupta <fullugupta1234@gmail.com>
2026-08-23 08:41:37 +0530

    Time: 19 ms (53.53%) | Memory: 43.4 MB (12.24%) - LeetSync

---
+++ 4346-subsequence-after-one-replacement/subsequence-after-one-replacement.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canMakeSubsequence(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        if(n>m) return false;
+
+        if(n==0) return true;
+
+        vector<int>left(n, -1), right(n, -1);
+        int p=0;
+        for(int i=0; i<n; i++){
+            while(p<m && s[i] != t[p]) p++;
+
+            if(p==m) break;
+            left[i] = p++;
+        } 
+
+        if(left[n-1]!=-1) return true;
+
+        p = m-1;
+        for(int i=n-1; i>=0; i--){
+            while(p>=0 && s[i] != t[p]) p--;
+
+            if(p<0) break;
+            right[i] = p--;
+        } 
+
+        for(int i=0; i<n; i++){
+            if((i==0|| left[i-1]!=-1) && (i==n-1|| right[i+1]!=-1)){
+                int L=(i==0)?-1:left[i-1];
+                int R=(i==n-1)?m:right[i+1];
+
+                if(L+1<R) return true;
+            }
+        } 
+        return false;
+    }
+};
