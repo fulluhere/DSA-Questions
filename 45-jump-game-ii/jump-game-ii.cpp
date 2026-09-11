d683b4c46a7be5872144c013bfdfd8e8a8c5ebde
Priyanka Gupta <fullugupta1234@gmail.com>
2026-09-11 20:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (78.58%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int near =0, far = 0;
+        int jumps = 0;
+        while(far<nums.size()-1){
+            int farthest = 0;
+            for(int i=near; i<=far; i++){
+                farthest = max(farthest, i+nums[i]);
+            }
+            near = far + 1;
+            far = farthest;
+            jumps++;
+        }
+
+        return jumps;
+
+    }
+};
